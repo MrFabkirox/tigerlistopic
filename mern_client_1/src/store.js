50615a7282690f7215be1b747925746f1607b109
Fabrice Esope <fabrice.esope@gmail.com>
2018-12-05 17:50:43 +0800

    client not starting on prod

--- mern_client_1/src/store.js
+++ mern_client_1/src/store.js
@@ -10,4 +10,13 @@ const store = createStore(rootReducer, initialState, compose(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
+// const composeEnhancers =  typeof window === 'object' &&  window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ ?
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+// 
+// const composingMiddlewareAndDevTools = composeEnhancers(applyMiddleware(...middleware));
+// 
+// const store = createStore(rootReducer, initialState, composingMiddlewareAndDevTools);
+// 
+// console.log(store.getState());
+
 export default store
